5020c3af0b657557da1c916a5220149c2514b69c
Egor <kycuj.egor2020@gmail.com>
2026-07-02 19:35:51 +0300

    feat: added json block into main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::symbols::border;
 // use crossterm::style::Stylize;
-use ratatui::text::Line;
+use ratatui::text::{Line, Text, ToText};
 use ratatui::style::Stylize;
-use ratatui::widgets::{Paragraph, Widget};
+use ratatui::widgets::{Block, Paragraph, Widget};
 use ratatui::{DefaultTerminal, Frame, Terminal};
 use serde_json::json;
 use std::fs::File;
@@ -12,7 +13,8 @@ use std::path::Path;
 
 #[derive(Default)]
 struct App {
-    exit: bool
+    exit: bool,
+    json_text: serde_json::Value
 }
 
 impl App {
@@ -40,6 +42,10 @@ impl App {
         Ok(())
     }
 
+    fn render_json(&self) -> Block<'static> {
+        Block::bordered().title(Line::from(self.json_text.to_string())).border_set(border::EMPTY)
+    }
+
     fn handle_key_events(&mut self, key_event: KeyEvent) {
         match key_event.code {
             KeyCode::Char('q') => self.exit = true,
@@ -51,16 +57,30 @@ impl App {
 impl Widget for &App {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer) {
         let title = Line::from(" JSON reader app ".bold());
+        let bottom_title = Line::from(" Press 'Q' to exit ".bold());
+        let inner_block = self.render_json();
+        let outer_block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(bottom_title.centered())
+            .border_set(border::THICK);
 
-        Paragraph::new(title).render(area, buf);
-        
+        let outer_area = outer_block.inner(area);
+
+        outer_block.render(area, buf);
+
+        Paragraph::new("Title").block(inner_block).render(outer_area, buf);
+
     }
     
 }
 
 fn main() -> std::io::Result<()> {
-    // let path = Path::new("./some.json");
-    // let file = File::open(path)?;
-    // let file_buff = BufReader::new(file);
-    ratatui::run(|terminal| App::default().run(terminal))
+    let path = Path::new("src/some.json");
+    let file = File::open(path)?;
+    let file_buff = BufReader::new(file);
+    let json_data: serde_json::Value = serde_json::from_reader(file_buff)?;
+
+    let mut app = App {exit: false, json_text: json_data};
+
+    ratatui::run(|terminal| app.run(terminal))
 }
